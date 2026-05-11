ac29ba262f7c22a7a45ba8652d5a92d8823166ff
kuri <piromasa1234@gmail.com>
2026-05-11 12:19:29 +0900

    Complete day 3

---
+++ day3.py
@@ -0,0 +1,15 @@
+# Day 3: for文・while文
+
+# 問題:
+# for と range() を使って、1から5まで順番に表示する。
+
+# ヒント:
+# range(1, 6) は 1, 2, 3, 4, 5 を順番に取り出す。
+
+# ここから自分で書いてみよう。
+for i in range(1, 6):
+    print(i)
+c = 1
+while c <= 5:
+    print(c)
+    c += 1
